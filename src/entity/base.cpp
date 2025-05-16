0e0a994009f00a4b6bbb3af169177f648b2afce8
#Guigui <hastag.guigui@outlook.com>
2025-05-16 08:16:48 +0200

    Basic entity

---
+++ src/entity/base.cpp
@@ -0,0 +1,13 @@
+
+#include "base.hpp"
+#include <iostream>
+
+EntityBase::EntityBase() {}
+
+void EntityBase::process(float deltatime){
+	//std::cout << "test" << std::endl;
+}
+
+void EntityBase::draw() {
+
+}
