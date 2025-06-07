59194beaee9f598d8f5cc2afd34ea3815cf9e32c
gogow <hugo.viard73@gmail.com>
2025-06-07 14:46:23 +0200

    code de Hugo et de son chat

---
+++ src/entity/enemy_slayer.cpp
@@ -0,0 +1,58 @@
+#include "enemy_slayer.h"
+#include <cmath>
+
+EnemySlayer::EnemySlayer(int screenWidth, int screenHeight, float speed, float size, Color color)
+    : speed(speed), size(size), color(color), dead(false)
+{
+    int side = GetRandomValue(0, 3);
+
+    switch (side) {
+    case 0:
+        position = { (float)GetRandomValue(0, screenWidth), -size };
+        break;
+    case 1:
+        position = { (float)GetRandomValue(0, screenWidth), (float)screenHeight + size };
+        break;
+    case 2:
+        position = { -size, (float)GetRandomValue(0, screenHeight) };
+        break;
+    case 3:
+        position = { (float)screenWidth + size, (float)GetRandomValue(0, screenHeight) };
+        break;
+    }
+}
+
+void EnemySlayer::update(float deltaTime, const Player& player) {
+    Vector2 target = player.GetPosition();
+    Vector2 direction = { target.x - position.x, target.y - position.y };
+
+    float length = sqrtf(direction.x * direction.x + direction.y * direction.y);
+    if (length != 0) {
+        direction.x /= length;
+        direction.y /= length;
+    }
+
+    position.x += direction.x * speed * deltaTime;
+    position.y += direction.y * speed * deltaTime;
+}
+
+void EnemySlayer::draw() const {
+    DrawRectangle(static_cast<int>(position.x), static_cast<int>(position.y),
+        static_cast<int>(size), static_cast<int>(size), color);
+}
+
+Vector2 EnemySlayer::GetPosition() const {
+    return position;
+}
+
+float EnemySlayer::GetSize() const {
+    return size;
+}
+
+void EnemySlayer::MarkAsDead() {
+    dead = true;
+}
+
+bool EnemySlayer::IsDead() const {
+    return dead;
+}
