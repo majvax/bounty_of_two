a3f3371cbcbc2397c18068096bebd7a5b30e9f3d
majvax <77504686+majvax@users.noreply.github.com>
2025-06-10 21:08:46 +0200

    Update enemy_stats.hpp

--- src/entity/enemy_stats.hpp
+++ src/entity/enemy_stats.hpp
@@ -4,15 +4,12 @@
 class EnemyStats
 {
 private:
-    int life;
     float attack_speed;
     float speed;
     float health;
 
 public:
     GETTER_SETTER(float, Health, health)
-    GETTER_SETTER(float, Attack_speed, attack_speed)
+    GETTER_SETTER(float, AtqSpeed, attack_speed)
     GETTER_SETTER(float, Speed, speed)
-    GETTER_SETTER(float, Size, size)
-
 };
