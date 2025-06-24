1485aecd997bdb345eecd03a338ace3fc5167369
#Guigui <hastag.guigui@outlook.com>
2025-06-24 16:51:08 +0200

    système d'xp quand on tue des ennemis

--- src/scene/scene_continuous_spawn.hpp
+++ src/scene/scene_continuous_spawn.hpp
@@ -4,7 +4,11 @@ class SceneContinuousSpawn : public Scene {
     int max_enemies_on_screen;
     int window_width;
     int window_height;
+    Renderer renderer;
+    GameState game_state;
     int score;
+    int exp;
+    int exp_pts;
 public:
     SceneContinuousSpawn(int width, int height, int max_enemies_on_screen);
     void update(float deltatime, ImGui::Context& ctx) override;
